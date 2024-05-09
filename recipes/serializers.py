a00e65203ad37662be6d7fab9e76005576fbe6e4
jdmarvin7 <marvin.destin@softfocus.com.br>
2024-05-08 23:38:40 -0300

    Mais sobre serializer validate, validation e sobreescrever metodos save, update etc...

--- recipes/serializers.py
+++ recipes/serializers.py
@@ -1,10 +1,24 @@
+from collections import defaultdict
 from rest_framework import serializers
 
-from recipes.models import Category
+from authors.validators import AuthorRecipeValidator
+from recipes.models import Category, Recipe
 from tag.models import Tag
 
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
 
-class RecipeSerializer(serializers.Serializer):
+
+class RecipeSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True)
+
+    class Meta:
+        model = Recipe
+        exclude = ['id',]
+
+    """
     id = serializers.UUIDField()
     title = serializers.CharField(max_length=255)
     description = serializers.CharField(max_length=255)
@@ -22,9 +36,48 @@ class RecipeSerializer(serializers.Serializer):
         queryset=Tag.objects.all(),
         many=True
     )
+    tag_links = serializers.HyperlinkedRelatedField(
+        many=True,
+        source='tags',
+        queryset=Tag.objects.all(),
+        view_name='recipes:api_recipes_detail'
+    )
 
-    # def get_preparation(self, recipe):
-    #     return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+    tags = TagSerializer(many=True)
+    def get_preparation(self, recipe):
+        return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+    """
 
     def any_method_name(self, recipe):
         return f'{recipe.preparation_time} {recipe.preparation_time_unit}'
+    
+    def validate(self, attrs):
+        super_validate = super().valida(attrs)
+        AuthorRecipeValidator(
+            data=attrs,
+            ErrorClass=serializers.ValidationError,
+        )
+
+        # cd = attrs
+
+        # _my_errors = defaultdict(list)
+
+        # title = cd.get('title')
+        # description = cd.get('description')
+
+        # if title == description:
+        #     _my_errors['title'].append('Cannot be equal to description')
+        #     _my_errors['description'].append('Cannot be equal to title')
+
+        # if _my_errors:
+        #     raise serializers.ValidationError(_my_errors)
+
+        return super_validate
+    
+    def validate_title(self, value):
+        title = value
+
+        if len(title) < 5:
+            raise serializers.ValidationError('Must have at least 5 chars.')
+
+        return title
--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,15 +1,29 @@
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from ..models import Recipe
 from ..serializers import RecipeSerializer
 from django.shortcuts import get_object_or_404
 
-@api_view(http_method_names=['get'])
+@api_view(http_method_names=['get', 'post', 'put'])
 def recipe_list(request):
-    model = Recipe.objects.all()
-    serializer = RecipeSerializer(model, many=True).data
-    return Response(serializer)
+    if request.method == 'GET':
+        model = Recipe.objects.all()
+        serializer = RecipeSerializer(model, many=True).data
+        return Response(serializer)
+    
+    elif request.method == 'POST':
+        serializer = RecipeSerializer(data=request.data)
+        # serializer.is_valid(raise_exception=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.validated_data,
+                status=status.HTTP_201_CREATED,
+            )
+        
+        return Response(serializer.errors, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 @api_view(http_method_names=['get'])
 def recipe_api_detail(request, pk):
